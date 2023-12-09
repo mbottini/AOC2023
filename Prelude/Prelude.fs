3c2a0cf8b2ecf012d5c9fdb09ad082bbf49b98be
Michael Bottini <mbottini1@gmail.com>
2023-12-08 21:44:20 -0800

    Add more prelude functions

--- Prelude/Prelude.fs
+++ Prelude/Prelude.fs
@@ -4,8 +4,17 @@ module Prelude =
     let curry f = fun x y -> f (x, y)
     let curry3 f = fun x y z -> f (x, y, z)
 
+    let uncurry f = fun (x, y) -> f x y
+
     let flip f = fun x y -> f y x
     let ignore2 _ _ = ()
+
+    let split (delim: char) (s: string) =
+        s.Split(
+            delim,
+            System.StringSplitOptions.RemoveEmptyEntries
+            ||| System.StringSplitOptions.TrimEntries
+        )
 
     let allMatches regex matcher s =
         (System.Text.RegularExpressions.Regex regex).Matches s |> Seq.map matcher
